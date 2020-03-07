fc5f280bfe22c5e4857ded9d4f9a5b899cc504c2
Munkh-Erdene Munkh-Erdene <tab.munkherdene@gmail.com>
2020-03-08 02:12:22 +0900

    Style added

--- next.config.js
+++ next.config.js
@@ -1,28 +1,47 @@
-/* eslint-disable */
-const withCss = require("@zeit/next-css");
+const withPlugins = require("next-compose-plugins");
+const withCSS = require("@zeit/next-css");
+const withSass = require("@zeit/next-sass");
+const withLess = require("@zeit/next-less");
 
-module.exports = withCss({
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      const antStyles = /antd\/.*?\/style\/css.*?/;
-      const origExternals = [...config.externals];
-      config.externals = [
-        (context, request, callback) => {
-          if (request.match(antStyles)) return callback();
-          if (typeof origExternals[0] === "function") {
-            origExternals[0](context, request, callback);
-          } else {
-            callback();
+if (typeof require !== "undefined") {
+  require.extensions[".less"] = file => {};
+}
+
+module.exports = withCSS(
+  withLess(
+    withSass({
+      lessLoaderOptions: { javascriptEnabled: true },
+      webpack: (config, { isServer }) => {
+        if (isServer) {
+          const antStyles = /antd\/.*?\/style.*?/;
+          const origExternals = [...config.externals];
+          config.externals = [
+            (context, request, callback) => {
+              if (request.match(antStyles)) return callback();
+              if (typeof origExternals[0] === "function") {
+                origExternals[0](context, request, callback);
+              } else {
+                callback();
+              }
+            },
+            ...(typeof origExternals[0] === "function" ? [] : origExternals)
+          ];
+          config.module.rules.unshift({
+            test: antStyles,
+            use: "null-loader"
+          });
+        }
+        config.module.rules.push({
+          test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
+          use: {
+            loader: "url-loader",
+            options: {
+              limit: 100000
+            }
           }
-        },
-        ...(typeof origExternals[0] === "function" ? [] : origExternals)
-      ];
-
-      config.module.rules.unshift({
-        test: antStyles,
-        use: "null-loader"
-      });
-    }
-    return config;
-  }
-});
+        });
+        return config;
+      }
+    })
+  )
+);
